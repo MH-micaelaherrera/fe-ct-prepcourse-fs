90ac5f6e76b64c729265b0de737786278551bfe3
mh micaela herrera <micaelanaraherrera@gmail.com>
2024-07-16 20:11:15 -0300

    ejercicios C6

--- C6 Bucles/ejercicios/06.js
+++ C6 Bucles/ejercicios/06.js
@@ -2,6 +2,9 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
+  return (num>=100 && num<=999 || num>=-100 && num<=-999)
 }
+console.log(tieneTresDigitos(15));
+console.log(tieneTresDigitos(350));
 
 module.exports = tieneTresDigitos;
--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,16 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+  // var multiplicacion =1
+  // multiplicacion = multiplicacion + 1;
+  var acum=1
+  for(var i=a; i<=b; i++) {
+    acum = acum * i 
+  }
+  if(acum===-0) {
+    return 0
+  } 
+  return Math.abs(acum)
 }
 
 module.exports = productoEntreNúmeros;
